230e66d91f022efd589190fd253ce5bd908b081c
Tom Bleher <72304996+tom-bleher@users.noreply.github.com>
2026-04-15 18:03:48 +0300

    Upgrade EM physics to option3
    Switch G4EmStandardPhysics_option1 to option3 and update default cut value.

--- src/PhysicsList.cc
+++ src/PhysicsList.cc
@@ -6,14 +6,14 @@
  * @brief Minimal EM physics with step limiter and default production cut.
  */
 #include "PhysicsList.hh"
-#include "G4EmStandardPhysics_option1.hh"
+#include "G4EmStandardPhysics_option3.hh"
 #include "G4StepLimiterPhysics.hh"
 #include "G4SystemOfUnits.hh"
 
 PhysicsList::PhysicsList() {
-    SetDefaultCutValue(50.0 * CLHEP::micrometer);
+    SetDefaultCutValue(5.0 * CLHEP::micrometer);
 
-    RegisterPhysics(new G4EmStandardPhysics_option1());
+    RegisterPhysics(new G4EmStandardPhysics_option3());
 
     RegisterPhysics(new G4StepLimiterPhysics());
 }
